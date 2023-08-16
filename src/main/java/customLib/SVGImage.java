ee529e9b791c29d893cf9adf53c44e6d47b4e1e7
Banh Mi <longkim223300@gmail.com>
2023-08-16 19:22:42 +0700

    Update Image SVG

---
+++ src/main/java/customLib/SVGImage.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customLib;
+
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author BanhMi88
+ */
+public class SVGImage extends JLabel{
+    private FlatSVGIcon svgIcon;
+    
+    public void setSvgImage(String fileName, int width, int height) {
+        svgIcon = new FlatSVGIcon(getSource.Image(fileName), width, height);
+        setIcon(svgIcon);
+    }
+    
+}
